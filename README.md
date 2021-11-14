91bebf3a75f33e43206361f4a3ddd78f6fc0c3ea
PawaOx4th <kanpim5@gmail.com>
2021-11-14 12:37:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-vxky91
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-vxky91)
---
+++ index.ts
@@ -0,0 +1,15 @@
+interface Observer {
+  next: (value: any) => void;
+  error: (value: any) => void;
+  complete: () => void;
+}
+
+const observer: Observer = {
+  next: (value: any) => console.log('next :', value),
+  error: (err: any) => console.error('error :', err),
+  complete: () => console.log('complete !!'),
+};
+
+observer.next('Hey Pawa.');
+observer.error('Eror'); 
+observer.complete();
